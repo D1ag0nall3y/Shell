5ddf5f1a8e9039b2c5ffe98b082f6f4d2c4efae6
jgt68 <jturnage@me.com>
2016-05-25 05:47:14 -0500

    Set ARD Fields
    Set Variables for ARD fields in the Shared section of System preferences

---
+++ ARD Fields.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Written by: Josh Turnage
+# 05/25/16
+
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -computerinfo -set1 -1 "Variable"
