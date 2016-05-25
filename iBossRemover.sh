3adb0394627d03fccf5f4d64de92701457e375c0
jgt68 <jturnage@me.com>
2016-05-25 05:27:56 -0500

    Remove iBoss files
    Self Explanatory

---
+++ iBossRemover.sh
@@ -0,0 +1,7 @@
+Sudo rm /bin/IBMC
+Sudo rm /bin/iboss.mobile.agent.sh
+Sudo rm /bin/iboss.mobile.reload.sh
+Sudo rm /bin/iboss.register.sh
+Sudo rm /Library/LaunchDaemons/iboss.mobile.agent.plist
+Sudo rm /Library/LaunchDaemons/iboss.mobile.reload.plist
+Sudo rm /Library/LaunchDaemons/iboss.register.plist
