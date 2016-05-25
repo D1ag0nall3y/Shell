d2d4eedccf7c1d2e9cced12c684f8a79f1083553
jgt68 <jturnage@me.com>
2016-05-25 05:43:52 -0500

    Disable PDF Plugin Safari
    Self Explanatory

---
+++ DisableAdobePDFinSafari.sh
@@ -0,0 +1,7 @@
+
+## Disables Adobe PDF Plug-in for Safari
+
+sudo mkdir /Library/Internet\ Plug-Ins\ Disabled
+sudo mv /Library/Internet\ Plug-Ins/AdobePDFViewer.plugin /Library/Internet\ Plug-Ins\ Disabled/
+sudo mv /Library/Internet\ Plug-Ins/AdobePDFViewerNPAPI.plugin /Library/Internet\ Plug-Ins\ Disabled/
+
