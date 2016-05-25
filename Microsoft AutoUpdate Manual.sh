c45c23156432629b7df5ab4611e1359ea8ccdf5c
jgt68 <jturnage@me.com>
2016-05-25 05:27:29 -0500

    Set Microsoft autoupdate to Manual
    Self Explanatory

---
+++ Microsoft AutoUpdate Manual.sh
@@ -0,0 +1,2 @@
+defaults write /Users/$3/Library/Preferences/com.microsoft.autoupdate2.plist HowToCheck Manual
+defaults write /Users/$3/Library/Preferences/com.microsoft.autoupdate2.plist WhenToCheck 2
